05313ed1b1fe7ac7f151712d797037c104144020
kim-langer <kim.sophie.langer@hs-furtwangen.de>
2023-07-11 16:20:23 +0200

    zs

---
+++ Main.js
@@ -0,0 +1,17 @@
+/*Aufgabe: Abschlussarbeit Eisdealer
+Name: Kim Langer
+Matrikelnummer: 272232
+Quellen: -
+
+Anmerkungen:
+keine Zusammenarbeit(en)
+*/
+var finaltask;
+(function (finaltask) {
+    let canvas = document.querySelector('canvas#back');
+    let crc2 = canvas.getContext('2d');
+    window.addEventListener("load", handleLoad);
+    function handleLoad(_event) {
+    }
+})(finaltask || (finaltask = {}));
+//# sourceMappingURL=Main.js.map
---
+++ Main.ts
@@ -0,0 +1,23 @@
+/*Aufgabe: Abschlussarbeit Eisdealer
+Name: Kim Langer
+Matrikelnummer: 272232
+Quellen: -
+
+Anmerkungen: 
+keine Zusammenarbeit(en)
+*/
+namespace finaltask {
+
+    let canvas = document.querySelector('canvas#back');
+    let crc2 = canvas.getContext('2d');
+
+    window.addEventListener("load", handleLoad);
+
+    function handleLoad(_event: Event): void {
+
+
+
+
+        }
+
+}
